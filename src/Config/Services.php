b233e113dbd1fa4c5ec64d59bdae0b9f29778dd5
kenjis <kenji.uui@gmail.com>
2022-04-25 08:31:14 +0900

    refactor: remove intermediate variable

--- src/Config/Services.php
+++ src/Config/Services.php
@@ -30,8 +30,6 @@ class Services extends BaseService
             return static::getSharedInstance('settings', $config);
         }
 
-        $config ??= config('Settings');
-
-        return new Settings($config);
+        return new Settings($config ?? config('Settings'));
     }
 }
