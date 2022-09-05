79406edfb26a76d5422b570f508867fc4caa03be
daycry <7590335+daycry@users.noreply.github.com>
2022-09-05 10:10:07 +0200

    Update DatabaseHandler.php

--- src/Handlers/DatabaseHandler.php
+++ src/Handlers/DatabaseHandler.php
@@ -29,7 +29,7 @@ class DatabaseHandler extends ArrayHandler
      */
     public function __construct()
     {
-        $this->builder = $builder ?? db_connect(config('Settings')->database['group'])->table(config('Settings')->database['table']);
+        $this->builder = db_connect(config('Settings')->database['group'])->table(config('Settings')->database['table']);
     }
 
     /**
