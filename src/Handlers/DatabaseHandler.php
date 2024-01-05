9d91da7fa44e87336cb43d5360db315dd4f558b2
Lonnie Ezell <lonnieje@gmail.com>
2024-01-04 23:03:31 -0600

    Update DatabaseHandler.php

--- src/Handlers/DatabaseHandler.php
+++ src/Handlers/DatabaseHandler.php
@@ -93,7 +93,7 @@ class DatabaseHandler extends ArrayHandler
                     'context'    => $context,
                     'updated_at' => $time,
                 ]);
-        // ...otherwise insert it
+            // ...otherwise insert it
         } else {
             $result = $this->builder
                 ->insert([
